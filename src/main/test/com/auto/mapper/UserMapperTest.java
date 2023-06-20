078f02ed8f6d1f86b5476e67f26bdc6a62cd7d61
wangddwei <1482466683@qq.com>
2023-06-21 00:59:25 +0800

    测试User增删改查

--- src/main/test/com/auto/mapper/UserMapperTest.java
+++ src/main/test/com/auto/mapper/UserMapperTest.java
@@ -10,14 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
-@ContextConfiguration(locations = "classpath:spring-mvc.xml")
+@ContextConfiguration(locations = "classpath:spring/spring-web.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UserMapperTest {
     @Autowired
     UserService userService;
+    @Autowired
+    UserMapper userMapper;
 
-    Logger logger = LoggerFactory.getLogger(UserMapper.class);
+    Logger logger = LoggerFactory.getLogger(UserMapperTest.class);
     /**
      * 测试登录
      */
@@ -25,10 +29,55 @@ public class UserMapperTest {
     public void selectUser() {
         User user = new User(
                 1,
-                "lisi",
+                "zhangsan",
                 "123456"
         );
         User loginUser = userService.getUserByUsernameAndPassword(user);
         logger.debug(loginUser + "");
+    }
+
+    /**
+     * 查询所有user
+     */
+    @Test
+    public void findAll(){
+        List<User> userList = userMapper.findAll();
+        logger.debug(userList + "");
+    }
+
+    /**
+     * 根据id查询User
+     */
+    @Test
+    public void getUserById(){
+        User user = userMapper.getUserById(1);
+        logger.debug(user + "");
+    }
+
+    /**
+     * 根据id删除User
+     */
+    @Test
+    public void deleteUserById() {
+        Integer rows = userMapper.deleteUserById(2);
+        logger.debug(rows + "");
+    }
+
+    /**
+     * 新增User
+     */
+    @Test
+    public void insertUser() {
+        Integer rows = userMapper.insertUser(new User(null,"赵七","123123"));
+        logger.debug(rows + "行执行了");
+    }
+
+    /**
+     * 更新User
+     */
+    @Test
+    public void updateUser() {
+        Integer rows = userMapper.updateUser(new User(2,"wangwu","123456"));
+        logger.debug(rows + "");
     }
 }
